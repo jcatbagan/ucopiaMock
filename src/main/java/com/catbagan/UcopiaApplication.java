9b764d8f19a1a38d947f553ea0722156473eccaa
Jenseric Catbagan <jenseric.catbagan@keyrus.com>
2016-11-09 09:54:25 +0100

    Refactoring  code for accept cross domain

--- src/main/java/com/catbagan/UcopiaApplication.java
+++ src/main/java/com/catbagan/UcopiaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
@@ -19,5 +20,15 @@ public class UcopiaApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(UcopiaApplication.class, args);
 	}
+	
+	   @Bean
+	    public WebMvcConfigurer corsConfigurer() {
+	        return new WebMvcConfigurerAdapter() {
+	            @Override
+	            public void addCorsMappings(CorsRegistry registry) {
+	                registry.addMapping("/**");
+	            }
+	        };
+	    }
 
 }
